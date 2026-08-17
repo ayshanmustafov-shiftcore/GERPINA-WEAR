46e9d66672b7e075e59c27933b532ddd82a3531c
Ayshan Mustafov <ayshan.mustafov@shiftcore.nl>
2026-08-17 19:09:30 +0200

    delete before push

--- app/men/page.js
+++
@@ -1,3 +0,0 @@
-import { Suspense } from 'react';
-import ShopExperience from '@/components/ShopExperience';
-export default function MenPage() { return <Suspense fallback={<div className="page-loading">Loading…</div>}><ShopExperience fixedAudience="men" /></Suspense>; }
--- components/CategoryPage.js
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import ShopExperience from '@/components/ShopExperience';
-
-export default function CategoryPage({ category }) {
-  return <ShopExperience fixedAudience={category} />;
-}
